c3c8e4d964541fd4d0ece450cd4484283050ee13
timmy-turner005 <timitalented005@gmail.com>
2026-02-12 16:39:26 +0100

    registration form in works

---
+++ src/app/components/RegistrationForm.jsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import SocialLogin from './SocialLogin';
+
+export default function RegistrationForm() {
+  return (
+    <>
+     
+      <form
+        className="my-5 flex flex-col items-center border p-3 border-gray-200 rounded-md"
+      >
+        <div className="my-2">
+          <label htmlFor="name">Name</label>
+          <input
+            className="border mx-2 border-gray-500 rounded"
+            type="text"
+            name="name"
+            id="name"
+          />
+        </div>
+        <div className="my-2">
+          <label htmlFor="email">Email Address</label>
+          <input
+            className="border mx-2 border-gray-500 rounded"
+            type="email"
+            name="email"
+            id="email"
+          />
+        </div>
+
+        <div className="my-2">
+          <label htmlFor="password">Password</label>
+          <input
+            className="border mx-2 border-gray-500 rounded"
+            type="password"
+            name="password"
+            id="password"
+          />
+        </div>
+
+        <button
+          type="submit"
+          className="bg-orange-300 mt-4 rounded flex justify-center items-center w-36"
+        >
+          Register
+        </button>
+      </form>
+      <SocialLogin />
+    </>
+  );
+}
--- src/app/register/page.jsx
+++ src/app/register/page.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
+import RegistrationForm from '../components/RegistrationForm';
 
 export default function RegisterPage() {
   return (
-    <div>RegisterPage</div>
+    <RegistrationForm />
   )
 }
