3d0ca081469b377828faf50fd760abc4f97e4863
timmy-turner005 <timitalented005@gmail.com>
2026-02-18 20:43:17 +0100

    login done

--- src/app/components/LoginForm.jsx
+++ src/app/components/LoginForm.jsx
@@ -1,72 +1,72 @@
-// "use client";
+"use client";
 
-// import SocialLogin from "./SocialLogin";
+import SocialLogin from "./SocialLogin";
 
-// import { doCredentialLogin } from "@/app/actions";
+import { doCredentialLogin } from "@/app/actions";
 
-// import { useState } from "react";
-// import { useRouter } from "next/navigation";
+import { useState } from "react";
+import { useRouter } from "next/navigation";
 
-// const LoginForm = () => {
-//   const router = useRouter();
-//   const [error, setError] = useState("");
+const LoginForm = () => {
+  const router = useRouter();
+  const [error, setError] = useState("");
 
-//   async function onSubmit(event) {
-//     event.preventDefault();
-//     try {
-//       const formData = new FormData(event.currentTarget);
+  async function onSubmit(event) {
+    event.preventDefault();
+    try {
+      const formData = new FormData(event.currentTarget);
 
-//       const response = await doCredentialLogin(formData);
+      const response = await doCredentialLogin(formData);
 
-//       if (!!response.error) {
-//         console.error(response.error);
-//         setError(response.error.message);
-//       } else {
-//         router.push("/home");
-//       }
-//     } catch (e) {
-//       console.error(e);
-//       setError("Check your Credentials");
-//     }
-//   }
+      if (!!response.error) {
+        console.error(response.error);
+        setError(response.error.message);
+      } else {
+        router.push("/home");
+      }
+    } catch (e) {
+      console.error(e);
+      setError("Check your Credentials");
+    }
+  }
 
-//   return (
-//     <>
-//       <div className="text-xl text-red-500">{error}</div>
-//       <form
-//         className="my-5 flex flex-col items-center border p-3 border-gray-200 rounded-md"
-//         onSubmit={onSubmit}
-//       >
-//         <div className="my-2">
-//           <label htmlFor="email">Email Address</label>
-//           <input
-//             className="border mx-2 border-gray-500 rounded"
-//             type="email"
-//             name="email"
-//             id="email"
-//           />
-//         </div>
+  return (
+    <>
+      <div className="text-xl text-red-500">{error}</div>
+      <form
+        className="my-5 flex flex-col items-center border p-3 border-gray-200 rounded-md"
+        onSubmit={onSubmit}
+      >
+        <div className="my-2">
+          <label htmlFor="email">Email Address</label>
+          <input
+            className="border mx-2 border-gray-500 rounded"
+            type="email"
+            name="email"
+            id="email"
+          />
+        </div>
 
-//         <div className="my-2">
-//           <label htmlFor="password">Password</label>
-//           <input
-//             className="border mx-2 border-gray-500 rounded"
-//             type="password"
-//             name="password"
-//             id="password"
-//           />
-//         </div>
+        <div className="my-2">
+          <label htmlFor="password">Password</label>
+          <input
+            className="border mx-2 border-gray-500 rounded"
+            type="password"
+            name="password"
+            id="password"
+          />
+        </div>
 
-//         <button
-//           type="submit"
-//           className="bg-orange-300 mt-4 rounded flex justify-center items-center w-36"
-//         >
-//           Ceredential Login
-//         </button>
-//       </form>
-//       <SocialLogin />
-//     </>
-//   );
-// };
+        <button
+          type="submit"
+          className="bg-orange-300 mt-4 rounded flex justify-center items-center w-36"
+        >
+          Ceredential Login
+        </button>
+      </form>
+      <SocialLogin />
+    </>
+  );
+};
 
-// export default LoginForm;
+export default LoginForm;
